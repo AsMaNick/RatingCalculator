ea2bd54eff13180fcff78609102ba0a66143b479
Matvey Aslandukov <matvej909@gmail.com>
2021-07-31 12:13:51 +0300

    Added possibility to use IsRated parameter

--- spreadsheet_app.js
+++ spreadsheet_app.js
@@ -118,7 +118,7 @@ function addStandingsToTheMainRating(data) {
   for (var i = 0; i < data.results.length; ++i) {
     var handle = getHandle(data.online_judge, data.results[i].user);
     if (handle in rowByHandle) {
-      sheet.getRange(rowByHandle[handle], column).setFormula(`=INDIRECT("R1C${column}"; FALSE) * '${data.sheet_name}'!G${i + 2}`);
+      sheet.getRange(rowByHandle[handle], column).setFormula(`=IF('${data.sheet_name}'!F${i + 2}; 1; Config!F2) * INDIRECT("R1C${column}"; FALSE) * '${data.sheet_name}'!G${i + 2}`);
     }
   }
 }
